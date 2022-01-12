fdcd31fa9d5af3d0fbd5b84acd5734f0372e54ac
Ryan Coulter <ryancoulter22@gmail.com>
2022-01-12 18:42:05 -0500

    Finish first section

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,29 @@ package main
 
 import "fmt"
 
+const french = "French"
+const spanish = "Spanish"
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
