516e5005136f04660c9c281219422142376ac858
Ryan Coulter <ryancoulter22@gmail.com>
2022-01-20 15:17:13 -0500

    Finish section on concurrency basics

---
+++ concurrency/check_websites.go
@@ -0,0 +1,27 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			// Send statement
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		// Recieve expression, takes value recieved
+		// from channel and puts it into a variable
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+	return results
+}
