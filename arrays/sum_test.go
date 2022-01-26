c1293ff58b0b9fbaf47d54f275b3c1865c537dbe
rjcoulter <ryancoulter22@gmail.com>
2022-01-26 15:43:53 -0500

    Rebase

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -10,7 +10,7 @@ func TestSum(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
-		want := 6
+		want := 6 
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
