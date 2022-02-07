b62b31191062a5750e534995786d1b10df5a77f4
rjcoulter <ryancoulter22@gmail.com>
2022-02-07 09:34:55 -0500

    Finish blogpost interface

--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -16,18 +16,41 @@ type StubFailingFS struct {
 func (s StubFailingFS) Open(name string) (fs.File, error) {
 	return nil, errors.New("oh no, i always fail")
 }
-func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
-	}
 
-	posts, _ := blogposts.NewPostsFromFS(fs)
-	// rest of test code cut for brevity
-	got := posts[0]
-	want := blogposts.Post{Title: "Post 1"}
-
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %+v, want %+v", got, want)
 	}
+}
+func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+R
+J
+C`
+	)
+
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, _ := blogposts.NewPostsFromFS(fs)
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body:        `Hello World`,
+	})
 }
